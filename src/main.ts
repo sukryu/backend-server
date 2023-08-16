36045cbee2a79b0e45beb6389c473e95acf6c750
sukryu <hjinhyeog162>
2023-08-17 00:33:06 +0900

    second commit

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new HttpExceptionFilter());
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  app.useGlobalPipes(new ValidationPipe({}));
+
+  const config = new DocumentBuilder()
+    .setTitle('Chat')
+    .setDescription('The chat API description')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.CHAT_PORT);
+}
+bootstrap();
